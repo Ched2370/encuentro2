c6d69737219522e7b315e867e2797840318da258
D3Po1 <103911012+D3Po1@users.noreply.github.com>
2023-08-09 01:19:19 -0300

    Se crea la clase ByGonza

---
+++ ProyectoClase2/src/proyectoclase2/ByGonza.java
@@ -0,0 +1,40 @@
+package proyectoclase2;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ByGonza extends JFrame {
+
+    public ByGonza() {
+        super("Hola a todos soy Gonza ❤️ !!!"); 
+
+        ImageIcon imagenIcono = new ImageIcon("ProyectoClase2/src/proyectoclase2/img/Img.png");
+        
+        JLabel imagenLabel = new JLabel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(imagenIcono.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        
+        JPanel panel = new JPanel(new BorderLayout());
+         panel.add(imagenLabel, BorderLayout.CENTER);
+        add(panel);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+  /*  Se comenta estas metodo ya que la llamada se hace desde el         main de la clase ProtectoClase2
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ByGonza();
+            }
+        });
+    }
+  */
+}
